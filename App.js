d80f5dabbc160e6ee30e21c37b20af81507498ac
chung nguyen <khoa_chung_89@outlook.com>
2021-05-27 06:06:46 +0700

    navigation demo

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //TODO: trying navigation :(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() => navigation.navigate("Profile", { name: "Jane" })}
+    />
+  );
+};
+const appCounter = 0;
+const ProfileScreen = ({ navigation, route }) => {
+  return (
+    <View>
+      <Text>This is {route.params.name}'s profile</Text>
+      <Button
+        title="Next Profile"
+        onPress={() =>
+          navigation.navigate("Profile", { name: "Jane " + Math.random() })
+        }
+      />
+      <Button title="Go Home" onPress={() => navigation.navigate("Home")} />
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
